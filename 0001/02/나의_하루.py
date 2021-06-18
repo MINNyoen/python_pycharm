eb0736888a6042fe4f8f5b9c3392643d892c4789
MINNyoen <78012724+MINNyoen@users.noreply.github.com>
2021-06-18 11:38:48 +0900

    Delete 0001 directory

--- 0001/02/나의_하루.py
+++
@@ -1,3 +0,0 @@
-from 매일 import *
-Day3 = everyday('와인공부','13','북촌')
-Day3.study1()
--- 0001/my/pymysql사용.py
+++
@@ -1,9 +0,0 @@
-from mysql연결 import *
-
-id = input("아이디를 입력하세요")
-pw = input("비밀번호를 입력하세요")
-name = input("이름을 입력하세요")
-tel = input("번호를 입력하세요")
-
-my1 = mysql(id,pw,name,tel)
-my1.delete()
